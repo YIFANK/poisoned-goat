51d1fd1f57c309704976e6d347278efb9649b74a
Yifan Knag <1052966756@qq.com>
2025-11-10 10:51:58 -0500

    fix finetune

--- finetune.py
+++ finetune.py
@@ -298,7 +298,7 @@ def train(
             fp16=True,
             logging_steps=10,
             optim="adamw_torch",
-            evaluation_strategy="steps" if val_set_size > 0 else "no",
+            eval_strategy="steps" if val_set_size > 0 else "no",
             save_strategy="steps",
             eval_steps=200 if val_set_size > 0 else None,
             save_steps=50,
